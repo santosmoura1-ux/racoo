7e56bfe4296aaaefef8c8f145e20841acf5ec191
MARIA HELENA DE LIMA CRISPIM <20261013010050@ifrn.local>
2026-07-09 21:58:04 -0300

    ajustando rotas

--- raco/urls.py
+++ raco/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("sobre/", views.sobre, name="sobre" ),
     path("kit/", views.kit, name ="kit"),
     path("convite/", views.convite, name ="convite"),
