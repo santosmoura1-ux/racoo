03a69c48d2d7baf7e775768f1c6544afee7b1625
MARIANA SANTOS DE MOURA <20261016270059@ifrn.local>
2026-07-15 22:02:21 -0300

    remove url

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path ('raco/', include("raco.urls")),
+    path ('', include("raco.urls")),
     path('admin/', admin.site.urls),
 ]
